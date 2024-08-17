3f0b3124604e65941900ddfe66e1745a20609cd1
Sollace <sollacea@gmail.com>
2024-08-17 19:58:06 +0100

    Fixed crash from entities without renderers and created a renderer for thrown bombs

---
+++ src/main/java/com/invasion/client/render/entity/RenderTnt.java
@@ -0,0 +1,41 @@
+package com.invasion.client.render.entity;
+
+import com.invasion.entity.EntityIMPrimedTNT;
+
+import net.minecraft.block.Blocks;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.block.BlockRenderManager;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.TntMinecartEntityRenderer;
+import net.minecraft.client.texture.SpriteAtlasTexture;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.RotationAxis;
+
+public class RenderTnt extends EntityRenderer<EntityIMPrimedTNT> {
+    private final BlockRenderManager blockRenderManager;
+
+    public RenderTnt(EntityRendererFactory.Context context) {
+        super(context);
+        this.shadowRadius = 0.5F;
+        this.blockRenderManager = context.getBlockRenderManager();
+    }
+
+    @Override
+    public void render(EntityIMPrimedTNT tntEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        matrixStack.push();
+        matrixStack.translate(0.0F, 0.5F, 0.0F);
+        matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(-90.0F));
+        matrixStack.translate(-0.5F, -0.5F, 0.5F);
+        matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(90.0F));
+        TntMinecartEntityRenderer.renderFlashingBlock(blockRenderManager, Blocks.TNT.getDefaultState(), matrixStack, vertexConsumerProvider, i, false);
+        matrixStack.pop();
+        super.render(tntEntity, f, g, matrixStack, vertexConsumerProvider, i);
+    }
+
+    @Override
+    public Identifier getTexture(EntityIMPrimedTNT tntEntity) {
+        return SpriteAtlasTexture.BLOCK_ATLAS_TEXTURE;
+    }
+}
